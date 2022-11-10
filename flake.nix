1f052e1ebb609340a966f106e81f22b4317e168e
James Leslie <jamesleslie@posteo.net>
2022-11-10 16:02:45 +0000

    Update flake to match nix-community/emacs-overlay structure

--- flake.nix
+++ flake.nix
@@ -2,14 +2,23 @@
   description = "A wrapper flake around the emacs-overlay, which fixes allows the nixpkgs input to be changed.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/f677051b8dc0b5e2a9348941c99eea8c4b0ff28f";
-    emacs-overlay.url = "github:nix-community/emacs-overlay/8ff1524472abef7c86c9e9c221d8969911074b4a";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, emacs-overlay, flake-utils }:
     {
-      overlays.default = final: prev: emacs-overlay.overlay final prev;
+      overlays = {
+        default = final: prev: emacs-overlay.overlays.default final prev;
+        emacs = final: prev: emacs-overlay.overlays.emacs final prev;
+        packages = final: prev: emacs-overlay.overlays.package final prev;
+      };
+      overlay = self.overlays.default;
+
     } // flake-utils.lib.eachDefaultSystem (
       system:
       let pkgs = nixpkgs.legacyPackages.${system};
